b3441ad915715533e64129d1f8bccfde3da13478
glustis2017 <glustis2017@ya.ru>
2017-02-19 18:35:43 +0500

    Button calls the scanner to scan QR-only
    Also some renamings for consistancy.

--- app/src/main/java/vazelin/qrdocsaver/StartingActivity.java
+++ app/src/main/java/vazelin/qrdocsaver/StartingActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -15,13 +16,15 @@ import com.google.zxing.integration.android.IntentResult;
 
 import java.io.File;
 
-public class StartingActivity extends AppCompatActivity {
+import static com.google.zxing.integration.android.IntentIntegrator.QR_CODE_TYPES;
+
+public class StartingActivity extends AppCompatActivity implements View.OnClickListener{
 
     File defaultDocPath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getPath() + "/Scans");
 
     EditText editText_pathToFolder;
     ListView listView_savedDocs;
-
+    Button buttonStartScan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -34,13 +37,22 @@ public class StartingActivity extends AppCompatActivity {
     }
 
     private void initializeUIAndLinks(){
-        editText_pathToFolder = (EditText)  findViewById(R.id.editText_PathToFolder);
+        editText_pathToFolder = (EditText)  findViewById(R.id.editText_pathToFolder);
         listView_savedDocs =    (ListView)  findViewById(R.id.listView_savedDocs);
+        buttonStartScan =       (Button)    findViewById(R.id.button_beginScan);
 
+        buttonStartScan.setOnClickListener(this);
         editText_pathToFolder.setText(defaultDocPath.getPath());
     }
 
     public void scanBarcode(View view) {
-        new IntentIntegrator(this).initiateScan();
+        new IntentIntegrator(this).initiateScan(QR_CODE_TYPES);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_beginScan){
+            scanBarcode(null);
+        }
     }
 }
