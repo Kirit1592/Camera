00034ce150b13edef6fa404abbaa4bafa32c686f
glustis2017 <glustis2017@ya.ru>
2017-02-20 01:00:38 +0500

    Camera helper is on the way!

---
+++ app/src/main/java/vazelin/qrdocsaver/CameraHelper.java
@@ -0,0 +1,194 @@
+package vazelin.qrdocsaver;
+
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.security.Security;
+import java.util.Arrays;
+
+
+
+
+
+import static android.content.Context.CAMERA_SERVICE;
+
+/**
+ * Created by Futurama on 2/20/2017.
+ */
+
+public class CameraHelper {
+
+    private CameraManager camManager = null;
+    private Size mPreviewSize = null;
+    private CameraDevice mCameraDevice = null;
+    private CaptureRequest.Builder mPreviewBuilder = null;
+    private CameraCaptureSession mPreviewSession = null;
+    private final static String TAG = "SimpleCamera";
+    private TextureView mTextureView = null;
+    private ImageReader ireader = null;
+
+    public void MakeAShot(String filePath, CameraManager managerToUse){
+        // TODO Auto-generated method stub
+        Log.i(TAG, "onSurfaceTextureAvailable()");
+
+        camManager = managerToUse;
+        try{
+            String cameraId = camManager.getCameraIdList()[0];
+            CameraCharacteristics characteristics = camManager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            mPreviewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+
+            camManager.openCamera(cameraId, mStateCallback, null);
+        }
+        catch(CameraAccessException e) {
+            e.printStackTrace();
+        }
+        catch (SecurityException e){
+            e.printStackTrace();
+        }
+    }
+
+    private TextureView.SurfaceTextureListener mSurfaceTextureListner = new TextureView.SurfaceTextureListener() {
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            // TODO Auto-generated method stub
+            //Log.i(TAG, "onSurfaceTextureUpdated()");
+
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width,
+                                                int height) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onSurfaceTextureSizeChanged()");
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onSurfaceTextureDestroyed()");
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width,
+                                              int height) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onSurfaceTextureAvailable()");
+
+            CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+            try{
+                String cameraId = manager.getCameraIdList()[0];
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                mPreviewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+
+                manager.openCamera(cameraId, mStateCallback, null);
+            }
+            catch(CameraAccessException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+    };
+
+
+
+    private CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+
+        @Override
+        public void onOpened(CameraDevice camera) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onOpened");
+            mCameraDevice = camera;
+
+            SurfaceTexture texture = mTextureView.getSurfaceTexture();
+            if (texture == null) {
+                Log.e(TAG, "texture is null");
+                return;
+            }
+
+            texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            Surface surface = new Surface(texture);
+
+            try {
+                mPreviewBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            } catch (CameraAccessException e){
+                e.printStackTrace();
+            }
+
+            mPreviewBuilder.addTarget(surface);
+
+            try {
+                mCameraDevice.createCaptureSession(Arrays.asList(surface), mPreviewStateCallback, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            // TODO Auto-generated method stub
+            Log.e(TAG, "onError");
+
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            // TODO Auto-generated method stub
+            Log.e(TAG, "onDisconnected");
+
+        }
+    };
+
+    private CameraCaptureSession.StateCallback mPreviewStateCallback = new CameraCaptureSession.StateCallback() {
+
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onConfigured");
+            mPreviewSession = session;
+
+            mPreviewBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+            HandlerThread backgroundThread = new HandlerThread("CameraPreview");
+            backgroundThread.start();
+            Handler backgroundHandler = new Handler(backgroundThread.getLooper());
+
+            try {
+                mPreviewSession.setRepeatingRequest(mPreviewBuilder.build(), null, backgroundHandler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void onConfigureFailed(CameraCaptureSession session) {
+            // TODO Auto-generated method stub
+            Log.e(TAG, "CameraCaptureSession Configure failed");
+        }
+
+
+    };
+
+
+
+}
